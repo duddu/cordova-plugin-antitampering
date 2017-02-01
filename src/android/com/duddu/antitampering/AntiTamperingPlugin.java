9fae0981e9f0056074c06fcc5baae13bab00b329
Davide Doronzo <davide.doronzo@gmail.com>
2017-02-01 19:27:41 +0100

    Split hashes check logic from plugin class Added AssetsIntegrity class for Android

--- src/android/com/duddu/antitampering/AntiTamperingPlugin.java
+++ src/android/com/duddu/antitampering/AntiTamperingPlugin.java
@@ -1,35 +1,13 @@
 package com.duddu.antitampering;
 
-import android.content.res.AssetManager;
-import android.util.Base64;
-import android.util.ArrayMap;
-
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Map.Entry;
-
 
 public class AntiTamperingPlugin extends CordovaPlugin {
-
-    private AssetManager cordovaAssets;
-    private ArrayMap<String, String> AssetsHashes = new ArrayMap<String, String>();
-
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
-        cordovaAssets = cordova.getActivity().getAssets();
-    }
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
@@ -39,16 +17,7 @@ public class AntiTamperingPlugin extends CordovaPlugin {
                 public void run () {
                     PluginResult result;
                     try {
-                        for (Entry<String, String> entry : AssetsHashes.entrySet())
-                        {
-                            byte[] fileNameDecode = Base64.decode(entry.getKey(), 0);
-                            String fileName = new String(fileNameDecode, StandardCharsets.UTF_8);
-                            // Log.d("antitampering", fileName + " -> " + entry.getValue());
-                            if (entry.getValue() == null ||
-                                !entry.getValue().equals(createHash("www/" + fileName))) {
-                                throw new Exception("Hash for " + fileName + " doesn't match");
-                            }
-                        }
+                        AssetsIntegrity.check(cordova.getActivity().getAssets());
                         result = new PluginResult(PluginResult.Status.OK, true);
                     } catch (Exception e) {
                         result = new PluginResult(PluginResult.Status.ERROR, e.toString());
@@ -61,29 +30,6 @@ public class AntiTamperingPlugin extends CordovaPlugin {
 
         return false;
 
-    }
-
-    private String createHash(String fileName) throws IOException, NoSuchAlgorithmException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        InputStream file = cordovaAssets.open(fileName);
-        int nRead;
-        byte[] data = new byte[16384];
-        while ((nRead = file.read(data, 0, data.length)) != -1) {
-            buffer.write(data, 0, nRead);
-        }
-        buffer.flush();
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hashBytes = digest.digest(buffer.toByteArray());
-        StringBuffer hexString = new StringBuffer();
-        for (int i = 0; i < hashBytes.length; i++) {
-            if ((0xff & hashBytes[i]) < 0x10) {
-                hexString.append("0");
-            }
-            hexString.append(Integer.toHexString(0xFF & hashBytes[i]));
-        }
-        String hash = new String(hexString);
-        // Log.d("antitampering" + fileName, hash);
-        return hash;
     }
 
 }
---
+++ src/android/com/duddu/antitampering/AssetsIntegrity.java
@@ -0,0 +1,61 @@
+package com.duddu.antitampering;
+
+import android.content.res.AssetManager;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+class AssetsIntegrity {
+
+    private static final String MESSAGE_DIGEST_ALGORITHM = "SHA-256";
+    private static final String ASSETS_BASE_PATH = "www/";
+
+    public static void check(AssetManager assets) throws Exception {
+        for (Map.Entry<String, String> entry : assetsHashes.entrySet()) {
+            byte[] fileNameDecode = Base64.decode(entry.getKey(), 0);
+            String fileName = new String(fileNameDecode, StandardCharsets.UTF_8);
+            // Log.d("AntiTampering", fileName + " -> " + entry.getValue());
+            String filePath = ASSETS_BASE_PATH.concat(fileName);
+            InputStream file = assets.open(filePath);
+            String hash = createHash(file);
+            if (entry.getValue() == null || !entry.getValue().equals(hash)) {
+                throw new Exception("Hash for " + fileName + " doesn't match");
+            }
+        }
+    }
+
+    private static String createHash(InputStream file) throws IOException, NoSuchAlgorithmException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+        while ((nRead = file.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        MessageDigest digest = MessageDigest.getInstance(MESSAGE_DIGEST_ALGORITHM);
+        byte[] hashBytes = digest.digest(buffer.toByteArray());
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < hashBytes.length; i++) {
+            if ((0xff & hashBytes[i]) < 0x10) {
+                hexString.append("0");
+            }
+            hexString.append(Integer.toHexString(0xFF & hashBytes[i]));
+        }
+        // Log.d("AntiTampering", String(hexString));
+        return new String(hexString);
+    }
+
+    private static final Map<String, String> assetsHashes = Collections.unmodifiableMap(
+        new HashMap<String, String>()
+    );
+
+}

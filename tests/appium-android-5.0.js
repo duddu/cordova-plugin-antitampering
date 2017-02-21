143b407006a94df53f50533736ffca200c0edea2
Davide Doronzo <davide.doronzo@gmail.com>
2017-02-21 01:48:43 +0100

    Added appium tampering test for android

--- tests/appium-android-5.0.js
+++ tests/appium-android-5.0.js
@@ -23,15 +23,21 @@ var logging = function (driver) {
     });
 };
 
+var getWebViewContext = function (driver) {
+    return driver.contexts().then(function (contexts) {
+        return driver.context(contexts[1]);
+    });
+};
+
 var antiTamperingTest = function (callback) {
     return cordova.plugins.AntiTampering.verify(function (success) {
-        callback('succes -> ' + JSON.stringify(success));
+        callback('success -> ' + JSON.stringify(success));
     }, function (error) {
         callback('error -> ' + JSON.stringify(error));
     });
 };
 
-describe('AntiTampering Plugin Test', function () {
+describe('AntiTampering Plugin Test - Android', function () {
     this.timeout(300000);
     var driver;
     var allPassed = true;
@@ -59,7 +65,7 @@ describe('AntiTampering Plugin Test', function () {
                 tags: ['cordova-plugin-antitampering'],
                 tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
             })
-            .setImplicitWaitTimeout(3000);
+            .setImplicitWaitTimeout(5000);
     });
 
     after(function () {
@@ -74,15 +80,25 @@ describe('AntiTampering Plugin Test', function () {
         allPassed = allPassed && this.currentTest.state === 'passed';
     });
 
-    it('should be tampered', function () {
-        return driver
-            .contexts().then(function (contexts) {
-                console.log(contexts);
-                return driver.context(contexts[1]);
-            })
-            .setAsyncScriptTimeout(10000)
+    it('The Hello World cordova app should load successfully', function () {
+        return getWebViewContext(driver)
+            .title().should.eventually.equal('Hello World');
+    });
+
+    it('The file index.html should have been actually tampered with', function () {
+        return getWebViewContext(driver)
+            .waitForElementById('tampering')
+            .should.eventually.exist;
+    });
+
+    it('The plugin should be able to detect tampering on index.html', function () {
+        return getWebViewContext(driver)
+            .setAsyncScriptTimeout(20000)
             .executeAsync(antiTamperingTest, [])
             .then(function (result) {
+                if (!result.match('index.html has been tampered')) {
+                    throw new Error('The tampering was not detected');
+                }
                 return result;
             });
     });
